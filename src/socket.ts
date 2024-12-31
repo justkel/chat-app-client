6f5af296e044e734725e3df6194c1968936d95d5
justkel <77353055+justkel@users.noreply.github.com>
2024-12-31 16:56:25 +0100

    Created Socket.ts file

---
+++ src/socket.ts
@@ -0,0 +1,12 @@
+// socket.js
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:5002', {
+    reconnection: true,
+    reconnectionAttempts: Infinity,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    timeout: 20000,
+});
+
+export default socket;
